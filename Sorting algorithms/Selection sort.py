2bec3439959ad59ccccaa4212927d971f78ec481
IsmailKhalimov <lirikfeed049@gmail.com>
2024-02-15 19:21:31 +0300

    Delete some comments

--- Sorting algorithms/Selection sort.py
+++ Sorting algorithms/Selection sort.py
@@ -1,5 +1,4 @@
 def selection_sort(nums):
-    # Значение i соответствует кол-ву отсортированных значений
     for i in range(len(nums)):
         # Исходно считаем наименьшим первый элемент
         lowest_value_index = i
@@ -10,7 +9,6 @@ def selection_sort(nums):
         # Самый маленький элемент меняем с первым в списке
         nums[i], nums[lowest_value_index] = nums[lowest_value_index], nums[i]
 
-# Проверяем, что оно работает
 list_of_nums = [12, 8, 3, 20, 11]
 selection_sort(list_of_nums)
 print(list_of_nums)
